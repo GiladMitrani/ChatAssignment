f4d7799c130975a94063eb778024412a85cc4309
yrnklmn <yaronk22@gmail.com>
2015-12-07 14:39:10 +0200

    עדכון פרוטוקול שליחת הודעה

--- MessageProtocol.java
+++ MessageProtocol.java
@@ -10,5 +10,26 @@ package ChatAssignment;
  * @author YaronK
  */
 public class MessageProtocol {
+    final static int textMessage = 0, whoIsInMessage=1, disconnectMessage=2; 
+    String message;
+    int typeOfMessage;
+
+    public MessageProtocol(String message, int typeOfMessage) {
+        this.message = message;
+        this.typeOfMessage = typeOfMessage;
+    }
+
+    public static int getTextMessage() {
+        return textMessage;
+    }
+
+    public int getTypeOfMessage() {
+        return typeOfMessage;
+    }
+    
+    
+    
+    
+    
     
 }
